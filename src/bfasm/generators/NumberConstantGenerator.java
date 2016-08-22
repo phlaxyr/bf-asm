a5351c1e772ab3deb830354df178195aff697536
minerguy31 <minerguy31@gmail.com>
2016-08-22 14:28:24 -0600

    Other progress

---
+++ src/bfasm/generators/NumberConstantGenerator.java
@@ -0,0 +1,15 @@
+package bfasm.generators;
+
+public final class NumberConstantGenerator {
+	
+	private NumberConstantGenerator() {}
+	
+	public static String getConstant(int inp) {
+		//TODO: implement proper generation; current code is placeholder
+		
+		String ret = "";
+		for(int i=0;i<inp;i++)
+			ret += inp > 0 ? "+" : "-";
+		return ret;
+	}
+}
