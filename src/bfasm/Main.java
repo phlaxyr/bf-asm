87ad52c52605d6c4bcbd48be5e4f0c9ff11a80d9
minerguy31 <minerguy31@gmail.com>
2016-08-23 11:09:43 -0600

    Implement ADD

--- src/bfasm/Main.java
+++ src/bfasm/Main.java
@@ -1,16 +1,23 @@
 package bfasm;
 
+import bfasm.commands.AddCommand;
 import bfasm.commands.Command;
 import bfasm.commands.MovCommand;
 import bfasm.commands.SetCommand;
+import bfasm.generators.Optimizer;
 
 public class Main {
 	public static void main(String[] args) {
 		SetCommand.register();
 		MovCommand.register();
+		AddCommand.register();
 		
-		String command = ">+<"+Command.getCommand("SET 5 5").getBf()+Command.getCommand("MOV 5 0").getBf();
-		
+		String command = ">+<"+Command.getCommand("SET 5 8")
+				.getBf()+Command.getCommand("MOV 5 6")
+				.getBf()+Command.getCommand("ADD 5 6")
+				.getBf();
+
 		System.out.println(command);
-	}
+		System.out.println(Optimizer.removeRedundancy(command));
+	}	
 }
---
+++ src/bfasm/commands/AddCommand.java
@@ -0,0 +1,65 @@
+package bfasm.commands;
+
+import bfasm.generators.AddrGen;
+import bfasm.generators.NumberGen;
+
+public class AddCommand extends Command {
+
+	private int arg0;
+	private int arg1;
+	
+	private AddCommand() {
+		super(new int[]{0,0});
+	}
+	
+	public AddCommand(int[] args) {
+		super(args);
+	}
+
+	@Override
+	public String getBf() {
+		StringBuilder sb = new StringBuilder();
+		AddrGen ag = new AddrGen();
+		
+		ag.doNext(sb, "[-", AddrGen.getDataCell(arg1));
+		ag.doNext(sb, "+", AddrGen.getDataCell(arg0));
+		ag.doNext(sb, "+", AddrGen.getTempCell(arg1));
+		ag.doNext(sb, "]", AddrGen.getDataCell(arg1));
+		ag.doNext(sb, "[-<+>]", AddrGen.getTempCell(arg1));
+		ag.reset(sb);
+		
+		return sb.toString();
+	}
+
+	@Override
+	public String getMnemonic() {
+		return "ADD";
+	}
+
+	@Override
+	public int[] getArgs() {
+		return new int[]{arg0,arg1};
+	}
+
+	@Override
+	public Command setArgs(int[] args) {
+		
+		if(args.length != 2)
+			throw new RuntimeException("Incorrect number of arguments to ADD! Expected 2, got "+args.length);
+		
+		this.arg0 = args[0];
+		this.arg1 = args[1];
+		
+		return this;
+	}
+
+	@Override
+	public Command getClone() {
+		return new AddCommand();
+	}
+
+	public static void register() {
+		Command.registerCommand(new AddCommand());
+	}
+
+}
