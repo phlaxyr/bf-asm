5a974b8d1c2f74f441b130a8713166a98a7ec4a8
minerguy31 <minerguy31@gmail.com>
2016-08-22 21:10:31 -0600

    Optimize Optimizer

--- src/bfasm/generators/Optimizer.java
+++ src/bfasm/generators/Optimizer.java
@@ -4,23 +4,40 @@ public final class Optimizer {
 	
 	private Optimizer() {}
 	
-	//TODO: Optimize optimizer
-	
 	public static String removeRedundancy(String inp) {
-		while(canBeOptimized(inp)) {
-			inp = inp
-				.replace("<>", "")
-				.replace("><", "")
-				.replace("+-", "")
-				.replace("-+", "");
+		StringBuilder sb = new StringBuilder(" ");
+		for(char c : inp.toCharArray()) {
+			if((c == '>' && sb.charAt(sb.length() - 1) == '<') || (c == '<' && sb.charAt(sb.length() - 1) == '>'))
+				sb = sb.deleteCharAt(sb.length() - 1);
+			else
+				sb.append(c);
 		}
 		
-		return inp.replaceAll(">+$", "")
-				.replaceAll("<+$", "")
-				.replaceAll("^>+", "")
-				.replaceAll("^<+", "")
-				.replaceAll("\\++$", "")
-				.replaceAll("-+$", "");
+		sb = sb.deleteCharAt(0);
+		
+		String ret = null;
+		
+		for(int i = 0; i < sb.length(); i++) {
+			char c = sb.charAt(i);
+			if(c == '<' || c == '>') 
+				continue;
+			else {
+				ret = sb.substring(i);
+				break;
+			}
+		}
+		
+		for(int i = ret.length() - 1; i > 0; i--) {
+			char c = ret.charAt(i);
+			if(c == '<' || c == '>') 
+				continue;
+			else {
+				ret = ret.substring(0, i + 1);
+				break;
+			}
+		}
+		
+		return ret;
 	}
 	
 	public static boolean canBeOptimized(String inp) {
